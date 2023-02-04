527b724ffcc0ec94f15f8c7c2c017ffab933dcd1
Rowida46 <rowidanagah462000@gmail.com>
2023-02-04 15:09:06 +0200

    bla bla

--- dbEngine.sh
+++ dbEngine.sh
@@ -1,18 +1,3 @@
-#!/bin/bash
-
-# setup File 
-
-# 1-  handle main base dir
-
-
-## 2- add path -> handle env vals
-
-## Keep on reading commands from user ->
-
-### excute commands 
-
-baseDir=''
-#PATH=$PATH:$baseDir/create:$baseDir/validate:
 
 PATH="$baseDir:$baseDir/connect:$baseDir/create:$baseDir/databases:$baseDir/validate:$PATH"
 echo $PATH
